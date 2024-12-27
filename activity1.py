73624afd1e30272aae5de133e2c1aaf4ad471ffc
umermine <umermine4@gmail.com>
2024-12-27 19:06:01 +0500

    homework 8

---
+++ activity1.py
@@ -0,0 +1,6 @@
+#Adding variables
+base = float(input("Enter the base number: "))
+exponent = int(input("Enter the exponent: "))
+result = base ** exponent
+#Writing code
+print(f"{base} raised to the power of {exponent} is {result}")
